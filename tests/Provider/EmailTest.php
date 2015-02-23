de72921621c9d78e9600230251e9f0c694f1efe2
Craig Morris <craig.michael.morris@gmail.com>
2015-02-23 14:14:36 +0000

    More travis

--- tests/Provider/EmailTest.php
+++ tests/Provider/EmailTest.php
@@ -35,6 +35,10 @@ class EmailTest extends PHPUnit_Framework_TestCase
     public function testReport()
     {
         var_dump($_ENV);
+        var_dump(getenv('MAILTRAP_USERNAME'));
+        var_dump(getenv('MAILTRAP_PASSWORD'));
+        var_dump(getenv('MAILTRAP_INBOX'));
+        var_dump(getenv('MAILTRAP_TOKEN'));
 
         //  Arrange.
         $transport = new \Swift_SmtpTransport();
